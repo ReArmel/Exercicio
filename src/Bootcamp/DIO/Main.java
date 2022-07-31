4cf7873027b9155d51c66079e33fe231d0155bea
Renata <rearmel@icloud.com>
2022-07-31 13:42:23 -0400

    Initial commit

---
+++ src/Bootcamp/DIO/Main.java
@@ -0,0 +1,25 @@
+package Bootcamp.DIO;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println(" Exercicio calculadora");
+        Calculadora.soma (3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7,9);
+        Calculadora.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println(" Exercicio mensagem");
+        Mensagem.obterMensagem( 9 );
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        //Emprestimo
+        System.out.println(" Exercicio emprestimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+}
